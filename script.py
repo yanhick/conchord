b3911f581c4f5243e835d7150157d66b47e1eca7
yannick <yannick.dominguez@gmail.com>
2014-11-03 17:38:23 +0100

    added comments

--- script.py
+++ script.py
@@ -4,23 +4,35 @@ from sys import stdin
 from sys import stdout
 from sys import exit
 
+#display one line of open chord format
 def displayLine(parsed):
     stdout.write(" |" + parsed['chord']['name'] + "| " + parsed['lyrics'])
+
+    #don't start a new line unless the lyrics ends with a "."
     if "." in parsed['lyrics'][-1]:
         print ""
 
+#parse one line of open chord format, stop on any error
 def parse(line):
+
+    #split on double spaces
     data = line.split("  ")
+
     if len(data) < 2:
         exit('Missing chord name or lyrics in: ' + line)
+
     name = data[0].strip()
     lyrics = data[1].strip()
+
+    #check if chord's notes are provided
     if len(data) > 2:
         notes = data[2].strip()
         if len(notes) != 6:
             exit('Not the right number of strings for the notes: ' + notes)
     else:
         notes = None
+
+    #check if chord's fingering is provided
     if len(data) > 3:
         fingering = data[3].strip()
         if len(fingering) != 6:
@@ -38,6 +50,7 @@ def parse(line):
             }
 
 def main():
+    #parse and display each line
     for line in stdin:
         parsed = parse(line)
         displayLine(parsed)
