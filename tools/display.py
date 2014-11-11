50676139170d77d0eb5bea583983f03be384926a
yannick <yannick.dominguez@gmail.com>
2014-11-11 17:44:21 +0100

    fixed display tool

--- tools/display.py
+++ tools/display.py
@@ -9,17 +9,24 @@ argParser = argparse.ArgumentParser(description='Output pretty printed version o
         'format from stdin Output each line to stdout. Output any error to stderr')
 
 #display one line of conchord format
-def displayLine(parsed):
-    stdout.write(" |" + parsed['chord']['name'] + "| " + parsed['lyrics'])
+def displayLine(line):
 
-    #don't start a new line unless the lyrics ends with a "."
-    if "." in parsed['lyrics'][-1]:
-        print ""
+    if 'label' in line and line['label'] is not None:
+        stdout.write('\n' + line['label'] + '\n')
+
+    if 'chord-name' in line and line['chord-name'] is not None:
+        stdout.write(' |' + line['chord-name'] + '| ')
+
+    if 'lyrics' in line and line['lyrics'] is not None:
+        stdout.write(line['lyrics'])
+        #don't start a new line unless the lyrics ends with a '.'
+        if '.' in line['lyrics'][-1]:
+            print ''
 
 #display all the chords used in the song
 def displayChords(chords):
-    print "\n"
-    print "Chords used: "
+    print '\n'
+    print 'Chords used: '
 
     #display each chord used in the song
     for chordName, chord in chords.items():
@@ -28,41 +35,51 @@ def displayChords(chords):
 #display one chord
 def displayChord(chord):
 
-    #can't display if notes not provided
-    if chord['notes'] is None:
+    #can't display if name not provided
+    if 'chord-name' not in chord or chord['chord-name'] is None:
         return
 
-    print ""
-    print chord['name']
+    #can't display if notes not provided
+    if 'notes' not in chord or chord['notes'] is None:
+        return
+
+    print ''
+    print chord['chord-name']
 
     zippedChord = zip(
-            ['e', 'a', 'd', 'g', 'b', 'E'],
+            reversed(['e', 'a', 'd', 'g', 'b', 'E']),
             chord['notes'],
-            chord['fingerings'] if chord['fingerings'] is not None else [None for i in range(6)]
+            chord['fingerings'] if 'fingerings' in chord and chord['fingerings'] is not None else [None for i in range(6)]
             )
 
     for string, note, fingering in zippedChord:
-        line = list("------------------------")
+        line = list('------------------------')
         if note is not None:
             line[int(note)] = 'x' if fingering is None else fingering
-        print "".join(line)
+        print string + ' ' + ''.join(line)
 
 def main():
     #initial newline
-    print ""
+    print ''
 
     #store each used chords. Key is the chord's name
     chords = dict()
 
     #parse and display each line
     for line in stdin:
-        (errors, parsed) = parse(line)
-        displayLine(parsed)
-        chords[parsed['chord']['name']] = parsed['chord']
+        parsed = parse(line)
+        typed = dict()
+        for (field, item, errors) in parsed:
+            typed[field] = item
+
+        displayLine(typed)
+
+        if 'chord-name' in typed:
+            chords[typed['chord-name']] = typed
 
     displayChords(chords)
 
     #final newlines
-    print "\n"
+    print '\n'
 
 main()
