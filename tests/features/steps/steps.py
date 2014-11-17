3b3575adfb1710e00f68753efbe5cb0781efdbba
yannick <yannick.dominguez@gmail.com>
2014-11-17 02:30:30 +0100

    added features for parsing and some of the tests

--- tests/features/steps/steps.py
+++ tests/features/steps/steps.py
@@ -8,7 +8,6 @@ def step_impl(context):
 
 @then('I should get a data structure representing the line')
 def step_impl(context):
-    print parse(context.line)
     assert(parse(context.line)) == ([], {
             'lyrics': 'my lyrics',
             'chord-name': 'Am',
@@ -17,3 +16,58 @@ def step_impl(context):
             'label': None
         })
 
+@given('I parse a line with a label')
+def step_impl(context):
+    context.line = 'label'
+
+@then('I should get a data structure representing the label')
+def step_impl(context):
+    assert(parse(context.line)) == ([], {
+            'lyrics': None,
+            'chord-name': None,
+            'notes': None,
+            'fingerings': None,
+            'label': 'label'
+        })
+
+@given('I parse a line with a chord name')
+def step_impl(context):
+    context.line = '\tE'
+
+@then('I should get a data structure representing the chord name')
+def step_impl(context):
+    assert(parse(context.line)) == ([], {
+            'lyrics': None,
+            'chord-name': 'E',
+            'notes': None,
+            'fingerings': None,
+            'label': None
+        })
+
+@given('I parse a line with chords notes')
+def step_impl(context):
+    context.line = '\tE\t\t-221--'
+
+@then('I should get a data structure representing the notes')
+def step_impl(context):
+    assert(parse(context.line)) == ([], {
+            'lyrics': None,
+            'chord-name': 'E',
+            'notes': [None, 2, 2, 1, None, None],
+            'fingerings': None,
+            'label': None
+        })
+
+@given('I parse a line with chord\'s fingerings')
+def step_impl(context):
+    context.line = '\tE\t\t-221--\t-123--'
+
+@then('I should get a data structure representing the fingerings')
+def step_impl(context):
+    assert(parse(context.line)) == ([], {
+            'lyrics': None,
+            'chord-name': 'E',
+            'notes': [None, 2, 2, 1, None, None],
+            'fingerings': [None, 1, 2, 3, None, None],
+            'label': None
+        })
